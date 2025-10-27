65c28cd477925529bcb5cb33f74d7b8146c0508b
JericCabay10 <jericcabay10@gmail.com>
2025-10-27 08:42:27 +0000

    Task 2

---
+++ Chapter2/Task2.java
@@ -0,0 +1,18 @@
+package Chapter2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int x = 3110;
+
+        int zero = 0;
+        int one = 1;
+
+        float num = 2.0f;
+        boolean isTrue = true;
+
+        String output =  "H" + x + " " + "w" + zero + "r" + one + "d" + " " + num + " " + isTrue;
+        System.out.println(output);
+    }
+}
+
+//H3110 w0r1d
